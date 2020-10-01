42ec354a327dd911c921d60b72d0282049ba28c9
Anrusso23 <71799934+Anrusso23@users.noreply.github.com>
2020-09-30 18:27:49 -0700

    unfinished calculate age challenge

---
+++ challenges/calculate_age.py
@@ -0,0 +1,13 @@
+def calculate_age(current_year , birth_year):
+    age = current_year - birth_year
+    return age 
+   
+my_age = calculate_age(2049, - 2003)
+
+dads_age = calculate_age(2049, - 1953)
+
+x = my_age
+print(x)
+
+y = dads_age
+print(y)
---
+++ functions.py
@@ -0,0 +1,40 @@
+ # def greet_customer(special, sale_percent):
+ #     print("Our special is " + special)
+ #     print ("Everything is " + sale_percent + " off today.")
+ #     print("Have a great time shopping")
+
+# # greet_customer("peanut butter", "30%")  
+
+# def add_one(num):
+#     return num + 1
+
+# x = add_one(1)
+
+# print(x)
+
+# def add_one(num):
+#     if num % 2 != 0:
+#         return num + 1
+#     else:
+#         return num
+
+# def add_two(num):
+#     return num + 2
+
+def greet_customer(special):
+    return "Our special today is " + special
+
+print(greet_customer("banana yogurt"))
+
+def square_point_plus_ten (x,y):
+    z = 10 
+    print(z)
+    x_2 = x*x + z
+    y_2 = y*y + z
+    return x_2, y_2
+
+
+
+x = def square_point_plus_ten (1,4):
+
+print(x)
